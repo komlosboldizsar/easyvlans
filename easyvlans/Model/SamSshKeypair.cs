c8872bb9f3f530a97ed8cfce42d87a5ec51fa246
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-12-14 20:34:39 +0100

    Temporarily implement methods of SamSshKeypair by throwing NotImplementedException

--- easyvlans/Model/SamSshKeypair.cs
+++ easyvlans/Model/SamSshKeypair.cs
@@ -8,5 +8,9 @@ namespace easyvlans.Model
 {
     class SamSshKeypair : SwitchAccessMode 
     {
+        public override void Connect() => throw new NotImplementedException();
+        public override void Authenticate() => throw new NotImplementedException();
+        public override void WriteLine(string line) => throw new NotImplementedException();
+        public override Task<string[]> ReadLines() => throw new NotImplementedException();
     }
 }
