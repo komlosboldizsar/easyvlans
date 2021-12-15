89a2d96b5053f18a7522dff5501d261b28e56799
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-12-15 18:03:45 +0100

    Add LogDispatcher

---
+++ easyvlans/Logger/LogDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyvlans.Logger
+{
+    public class LogDispatcher
+    {
+        public delegate void LogMessageDelegate(LogMessageSeverity severity, string message);
+        public static event LogMessageDelegate LogMessage;
+        public static void Log(LogMessageSeverity severity, string message) => LogMessage?.Invoke(severity, message);
+        public static void E(string message) => Log(LogMessageSeverity.Error, message);
+        public static void W(string message) => Log(LogMessageSeverity.Warning, message);
+        public static void I(string message) => Log(LogMessageSeverity.Info, message);
+        public static void V(string message) => Log(LogMessageSeverity.Verbose, message);
+    }
+}
