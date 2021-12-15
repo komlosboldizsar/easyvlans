2f06f7c4d335802e19aeec4f89b4cb2ff4067fa7
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-12-15 18:26:04 +0100

    Store old log messages

--- easyvlans/Logger/LogDispatcher.cs
+++ easyvlans/Logger/LogDispatcher.cs
@@ -8,12 +8,24 @@ namespace easyvlans.Logger
 {
     public class LogDispatcher
     {
-        public delegate void LogMessageDelegate(LogMessageSeverity severity, string message);
-        public static event LogMessageDelegate LogMessage;
-        public static void Log(LogMessageSeverity severity, string message) => LogMessage?.Invoke(severity, message);
+
+        public delegate void NewLogMessageDelegate(LogMessageSeverity severity, string message);
+        public static event NewLogMessageDelegate NewLogMessage;
+
+        private static List<LogMessage> Messages { get; } = new List<LogMessage>();
+
+        public static void Log(LogMessageSeverity severity, string message)
+        {
+            Messages.Add(new LogMessage(severity, message));
+            NewLogMessage?.Invoke(severity, message);
+        }
+
         public static void E(string message) => Log(LogMessageSeverity.Error, message);
         public static void W(string message) => Log(LogMessageSeverity.Warning, message);
         public static void I(string message) => Log(LogMessageSeverity.Info, message);
         public static void V(string message) => Log(LogMessageSeverity.Verbose, message);
+
+
+
     }
 }
