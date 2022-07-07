c934b0b2687fa6778bdfbd0d2a97cd1eef4ba862
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-07-07 18:25:41 +0200

    Fix: call registerMethods() in MethodCollection.ctor()

--- easyvlans/Model/MethodCollection.cs
+++ easyvlans/Model/MethodCollection.cs
@@ -14,6 +14,7 @@ namespace easyvlans.Model
         public MethodCollection()
         {
             knownMethodFactoriesDictionary.Add(defaultMethodFactory.CreateInstance(null).Name, defaultMethodFactory);
+            registerMethods();
         }
 
         public TMethodInterface GetInstance(string name, Switch @switch)
