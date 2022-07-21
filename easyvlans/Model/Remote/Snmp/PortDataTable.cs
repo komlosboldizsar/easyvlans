6ac2826552430d58593efa20b943397bf3f7e707
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-07-21 10:21:15 +0200

    Use 1/2 (true/false) for TruthValues instead of 1/0 to meet SNMPv2-TC.mib

--- easyvlans/Model/Remote/Snmp/PortDataTable.cs
+++ easyvlans/Model/Remote/Snmp/PortDataTable.cs
@@ -74,7 +74,7 @@ namespace easyvlans.Model.Remote.Snmp
 
             public class HasComplexMembership : VariableDataProvider
             {
-                public override ISnmpData Get() => new Integer32(Item.HasComplexMembership ? 1 : 0);
+                public override ISnmpData Get() => new Integer32(Item.HasComplexMembership ? 1 : 2);
             }
 
             public class SetVlanMembershipStatus : VariableDataProvider
@@ -84,7 +84,7 @@ namespace easyvlans.Model.Remote.Snmp
 
             public class PendingChanges : VariableDataProvider
             {
-                public override ISnmpData Get() => new Integer32(Item.PendingChanges ? 1 : 0);
+                public override ISnmpData Get() => new Integer32(Item.PendingChanges ? 1 : 2);
             }
 
         }
