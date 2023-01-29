cf53b25c0cf432b47564c0ee4e54b0e49c0c4bc2
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2023-01-29 21:26:14 +0100

    Add module that uses CISCOSB-COPY-MIB to copy running-config to startup-config

---
+++ easyvlans-swopm-snmp-ciscosbcopy/Modules/SwopmSnmpCiscosbcopyModule.cs
@@ -0,0 +1,12 @@
+﻿using easyvlans.Model.SwitchOperationMethods;
+
+namespace easyvlans.Modules
+{
+    public class SwopmSnmpCiscosbcopyModule : IModule
+    {
+        public void Init()
+        {
+            SnmpPersistChangesMethodRegister.Instance.RegisterFactory(new SnmpPersistChangesCiscoSbCopyMethod.Factory());
+        }
+    }
+}
