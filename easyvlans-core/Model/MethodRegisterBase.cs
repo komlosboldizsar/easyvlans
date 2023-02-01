81623d884d514c394a8db441591e068cdf34e319
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2023-02-01 10:12:58 +0100

    Remove "(pre)known factories" from method registers

--- easyvlans-core/Model/MethodRegisterBase.cs
+++ easyvlans-core/Model/MethodRegisterBase.cs
@@ -5,8 +5,6 @@
         where TFactoryInterface : class, IMethod.IFactory<TMethodInterface>
     {
 
-        protected MethodRegisterBase() => registerKnownFactories();
-
         private readonly Dictionary<string, TFactoryInterface> registeredFactories = new();
 
         protected TFactoryInterface getFactory(string name)
@@ -16,14 +14,6 @@
         }
 
         public void RegisterFactory(TFactoryInterface factory) => registeredFactories.Add(factory.Code, factory);
-
-        private void registerKnownFactories()
-        {
-            foreach (TFactoryInterface factory in KnownFactories)
-                RegisterFactory(factory);
-        }
-
-        protected virtual TFactoryInterface[] KnownFactories { get; } = Array.Empty<TFactoryInterface>();
 
     }
 }
