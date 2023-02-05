2abfd40967cda5f86d19f7e81f91a7df10fc5fee
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2023-02-05 13:39:59 +0100

    Add extension methods for SNMP connections

---
+++ easyvlans-swopm-snmp/Model/SwitchOperationMethods/ISnmpConnectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Lextm.SharpSnmpLib;
+
+namespace easyvlans.Model.SwitchOperationMethods
+{
+    public static class ISnmpConnectionExtensions
+    {
+
+        public static Task SetAsync(this ISnmpConnection connection, Variable variable)
+            => connection.SetAsync(new List<Variable>() { variable });
+
+        public static Task SetAsync(this ISnmpConnection connection, ObjectIdentifier oid, ISnmpData data)
+            => connection.SetAsync(new List<Variable>() { new Variable(oid, data) });
+
+        public static Task SetAsync(this ISnmpConnection connection, string oidString, ISnmpData data)
+            => connection.SetAsync(new List<Variable>() { new Variable(new ObjectIdentifier(oidString), data) });
+
+    }
+}
