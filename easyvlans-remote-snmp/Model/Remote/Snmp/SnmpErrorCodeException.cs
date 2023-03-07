3795031ddf494f47ebec661b8868af6db742012b
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2023-03-07 11:59:30 +0100

    Add SnmpErrorCodeException and own message handler so all SNMP error codes can be used in the response

---
+++ easyvlans-remote-snmp/Model/Remote/Snmp/SnmpErrorCodeException.cs
@@ -0,0 +1,23 @@
+﻿using Lextm.SharpSnmpLib;
+
+namespace easyvlans.Model.Remote.Snmp
+{
+    internal class SnmpErrorCodeException : Exception
+    {
+
+        public ErrorCode ErrorCode { get; private init; }
+
+        public SnmpErrorCodeException(ErrorCode errorCode)
+            : base()
+            => ErrorCode = errorCode;
+
+        public SnmpErrorCodeException(ErrorCode errorCode, string message)
+            : base(message)
+            => ErrorCode = errorCode;
+
+        public SnmpErrorCodeException(ErrorCode errorCode, string message, Exception innerException)
+            : base(message, innerException)
+            => ErrorCode = errorCode;
+
+    }
+}
