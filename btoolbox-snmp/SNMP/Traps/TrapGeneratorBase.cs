346ace6cb1773a9f79317583d622ea8ec444de7f
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-04-13 20:04:44 +0200

    Add basic trap generator for non-table items

---
+++ btoolbox-snmp/SNMP/Traps/TrapGeneratorBase.cs
@@ -0,0 +1,39 @@
+﻿using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToolbox.SNMP.Traps
+{
+
+    public class SimpleTrapGenerator
+    {
+
+        private SnmpAgent _snmpAgent;
+        private string _code;
+        private TrapEnterprise _enterprise;
+
+        public SimpleTrapGenerator(SnmpAgent snmpAgent, string code, TrapEnterprise enterprise)
+        {
+            _snmpAgent = snmpAgent;
+            _code = code;
+            _enterprise = enterprise;
+        }
+
+        public SimpleTrapGenerator(SnmpAgent snmpAgent, string code, string enterpriseBase, int specificCode)
+            : this(snmpAgent, code, new TrapEnterprise(enterpriseBase, specificCode))
+        { }
+
+        protected void SendTrap(List<ScalarObject> payloadObjects)
+        {
+            List<Variable> payloadVariables = new();
+            payloadVariables = payloadObjects.Select(po => po.Variable).ToList();
+            _snmpAgent.SendTraps(_code, _enterprise, payloadVariables);
+        }
+
+    }
+
+}
