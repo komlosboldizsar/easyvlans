a93acb7c86da791ed4953364f9d33b8629e01f19
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-05-05 20:56:59 +0200

    Right click on VLAN select drop-down selects the default VLAN of the port if given

--- easyvlans/GUI/MainForm.PortRowManager.cs
+++ easyvlans/GUI/MainForm.PortRowManager.cs
@@ -38,61 +38,11 @@ namespace easyvlans.GUI
             public override void SubscribeControlEvents()
             {
                 _setVlanToComboBox.SelectedIndexChanged += setVlanToComboBoxSelectedIndexChangedHandler;
-                _setButton.Click += setButtonClickHandler;
+                _setVlanToComboBox.MouseDown += setVlanToComboBoxMouseDownHandler;
                 _setVlanToComboBox.DrawMode = DrawMode.OwnerDrawFixed;
-                _setVlanToComboBox.DrawItem += _setVlanToComboBox_DrawItem;
+                _setVlanToComboBox.DrawItem += setVlanToComboBoxDrawItemHandler;
+                _setButton.Click += setButtonClickHandler;
             }
-
-            private void _setVlanToComboBox_DrawItem(object sender, DrawItemEventArgs e)
-            {
-                if (e.Index == -1)
-                    return;
-                ComboBoxAdapter<Vlan>.ItemProxy comboBoxItem = _setVlanToComboBox.Items[e.Index] as ComboBoxAdapter<Vlan>.ItemProxy;
-                e.DrawBackground();
-                int defaultMarkVerticalSpace = (e.Bounds.Height - DEFAULT_MARK_SIZE) / 2;
-                Rectangle textBounds = e.Bounds with
-                {
-                    Width = e.Bounds.Width - DEFAULT_MARK_SIZE - 2 * defaultMarkVerticalSpace
-                };
-                using Pen textPen = new(e.ForeColor);
-                using Brush textBrush = new SolidBrush(e.ForeColor);
-                StringFormat stringFormat = new(StringFormat.GenericDefault)
-                {
-                    Alignment = StringAlignment.Far
-                };
-                Rectangle vlanIdFieldBounds = textBounds with
-                {
-                    Width = VLAN_ID_FIELD_WIDTH
-                };
-                Rectangle vlanNameFieldBounds = textBounds with
-                {
-                    X = textBounds.X + VLAN_ID_FIELD_WIDTH + FIELD_SEPARATOR_WIDTH,
-                    Width = textBounds.Width - VLAN_ID_FIELD_WIDTH - FIELD_SEPARATOR_WIDTH
-                };
-                e.Graphics.DrawString(comboBoxItem.Value?.ID.ToString(), e.Font, textBrush, vlanIdFieldBounds, stringFormat);
-                int px = textBounds.X + VLAN_ID_FIELD_WIDTH + FIELD_SEPARATOR_BAR_WIDTH;
-                Point p1 = new(px, textBounds.Top + 3);
-                Point p2 = new(px, textBounds.Bottom - 3);
-                e.Graphics.DrawLine(textPen, p1, p2);
-                e.Graphics.DrawString(comboBoxItem.Value?.Name.ToString(), e.Font, textBrush, vlanNameFieldBounds, StringFormat.GenericDefault);
-                if ((Item.DefaultVlan != null) && (comboBoxItem?.Value == Item.DefaultVlan))
-                {
-                    Rectangle defaultMarkRectangle = new(
-                        e.Bounds.X + e.Bounds.Width - defaultMarkVerticalSpace - DEFAULT_MARK_SIZE,
-                        (e.Bounds.Top + e.Bounds.Bottom - DEFAULT_MARK_SIZE) / 2,
-                        DEFAULT_MARK_SIZE,
-                        DEFAULT_MARK_SIZE
-                    );
-                    e.Graphics.FillRectangle(Brushes.Purple, defaultMarkRectangle);
-                }
-                e.DrawFocusRectangle();
-            }
-
-            private const int VLAN_ID_FIELD_WIDTH = 30;
-            private const int FIELD_SEPARATOR_SPACE_HALF_WIDTH = 2;
-            private const int FIELD_SEPARATOR_BAR_WIDTH = 1;
-            private const int FIELD_SEPARATOR_WIDTH = FIELD_SEPARATOR_SPACE_HALF_WIDTH * 2 + FIELD_SEPARATOR_BAR_WIDTH;
-            private const int DEFAULT_MARK_SIZE = 10;
 
             protected override void DebindItem()
             {
@@ -171,6 +121,66 @@ namespace easyvlans.GUI
                 if (!_setVlanToComboBox_changingAdapter)
                     setVlanToComboBoxSelections[Item] = _setVlanToComboBox.SelectedValue as Vlan;
             }
+
+            private void setVlanToComboBoxMouseDownHandler(object sender, MouseEventArgs e)
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    if (Item.DefaultVlan != null)
+                        _setVlanToComboBox.SelectByValue(Item.DefaultVlan);
+                }
+            }
+
+            private void setVlanToComboBoxDrawItemHandler(object sender, DrawItemEventArgs e)
+            {
+                if (e.Index == -1)
+                    return;
+                ComboBoxAdapter<Vlan>.ItemProxy comboBoxItem = _setVlanToComboBox.Items[e.Index] as ComboBoxAdapter<Vlan>.ItemProxy;
+                e.DrawBackground();
+                int defaultMarkVerticalSpace = (e.Bounds.Height - DEFAULT_MARK_SIZE) / 2;
+                Rectangle textBounds = e.Bounds with
+                {
+                    Width = e.Bounds.Width - DEFAULT_MARK_SIZE - 2 * defaultMarkVerticalSpace
+                };
+                using Pen textPen = new(e.ForeColor);
+                using Brush textBrush = new SolidBrush(e.ForeColor);
+                StringFormat stringFormat = new(StringFormat.GenericDefault)
+                {
+                    Alignment = StringAlignment.Far
+                };
+                Rectangle vlanIdFieldBounds = textBounds with
+                {
+                    Width = VLAN_ID_FIELD_WIDTH
+                };
+                Rectangle vlanNameFieldBounds = textBounds with
+                {
+                    X = textBounds.X + VLAN_ID_FIELD_WIDTH + FIELD_SEPARATOR_WIDTH,
+                    Width = textBounds.Width - VLAN_ID_FIELD_WIDTH - FIELD_SEPARATOR_WIDTH
+                };
+                e.Graphics.DrawString(comboBoxItem.Value?.ID.ToString(), e.Font, textBrush, vlanIdFieldBounds, stringFormat);
+                int px = textBounds.X + VLAN_ID_FIELD_WIDTH + FIELD_SEPARATOR_BAR_WIDTH;
+                Point p1 = new(px, textBounds.Top + 3);
+                Point p2 = new(px, textBounds.Bottom - 3);
+                e.Graphics.DrawLine(textPen, p1, p2);
+                e.Graphics.DrawString(comboBoxItem.Value?.Name.ToString(), e.Font, textBrush, vlanNameFieldBounds, StringFormat.GenericDefault);
+                if ((Item.DefaultVlan != null) && (comboBoxItem?.Value == Item.DefaultVlan))
+                {
+                    Rectangle defaultMarkRectangle = new(
+                        e.Bounds.X + e.Bounds.Width - defaultMarkVerticalSpace - DEFAULT_MARK_SIZE,
+                        (e.Bounds.Top + e.Bounds.Bottom - DEFAULT_MARK_SIZE) / 2,
+                        DEFAULT_MARK_SIZE,
+                        DEFAULT_MARK_SIZE
+                    );
+                    e.Graphics.FillRectangle(Brushes.Purple, defaultMarkRectangle);
+                }
+                e.DrawFocusRectangle();
+            }
+
+            private const int VLAN_ID_FIELD_WIDTH = 30;
+            private const int FIELD_SEPARATOR_SPACE_HALF_WIDTH = 2;
+            private const int FIELD_SEPARATOR_BAR_WIDTH = 1;
+            private const int FIELD_SEPARATOR_WIDTH = FIELD_SEPARATOR_SPACE_HALF_WIDTH * 2 + FIELD_SEPARATOR_BAR_WIDTH;
+            private const int DEFAULT_MARK_SIZE = 10;
 
             private async void setButtonClickHandler(object sender, EventArgs e) => await Item?.SetVlanTo(_setVlanToComboBox.SelectedValue as Vlan);
 
