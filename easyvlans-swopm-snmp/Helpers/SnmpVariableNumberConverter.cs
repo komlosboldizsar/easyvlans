f26bd5ae474a5eb61e063f47bfebcefbcecc5416
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-06-30 13:08:54 +0200

    Add helpers to extract data from SNMP variables that contain numbers

---
+++ easyvlans-swopm-snmp/Helpers/SnmpVariableNumberConverter.cs
@@ -0,0 +1,32 @@
+﻿using Lextm.SharpSnmpLib;
+
+namespace easyvlans.Helpers
+{
+    public static class SnmpVariableNumberConverter
+    {
+
+        public static bool ToInt(this Variable variable, Action<int> storeAction)
+        {
+            if (int.TryParse(variable.Data.ToString(), out int intData))
+            {
+                storeAction(intData);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ToUInt(this Variable variable, Action<uint> storeAction)
+        {
+            if (uint.TryParse(variable.Data.ToString(), out uint intData))
+            {
+                storeAction(intData);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ToTimeTicks(this Variable variable, Action<TimeTicks> storeAction)
+            => variable.ToUInt(ui => storeAction(new TimeTicks(ui)));
+
+    }
+}
