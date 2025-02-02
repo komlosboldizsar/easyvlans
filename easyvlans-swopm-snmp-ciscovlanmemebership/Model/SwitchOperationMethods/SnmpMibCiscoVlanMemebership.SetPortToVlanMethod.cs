b05bc53e612d670305bf9cce9d2d23e4868489ac
Bálint József Halla <hallabalint@edu.bme.hu>
2025-02-02 17:17:56 +0100

    read ready, setport not started napvégi

---
+++ easyvlans-swopm-snmp-ciscovlanmemebership/Model/SwitchOperationMethods/SnmpMibCiscoVlanMemebership.SetPortToVlanMethod.cs
@@ -0,0 +1,71 @@
+﻿using easyvlans.Helpers;
+using Lextm.SharpSnmpLib;
+
+namespace easyvlans.Model.SwitchOperationMethods
+{
+    internal sealed partial class SnmpMibCiscoVlanMemebership
+    {
+        internal partial class SetPortToVlanMethod : MethodBase, ISetPortToVlanMethod
+        {
+
+            public SetPortToVlanMethod(ISnmpConnection snmpConnection, object commonData)
+                : base(snmpConnection, commonData)
+            {
+                string variantName = ((CommonData)commonData).SetMembershipVariant;
+                if ((variantName == null) || !VARIANTS.TryGetValue(variantName, out _variant))
+                    _variant = DEFAULT_VARIANT;
+            }
+
+            private readonly IVariant _variant;
+
+            public async Task<bool> DoAsync(Port port, Vlan vlan)
+            {
+                List<Variable> pvidVariables = new(),
+                    egressToUnset = new(),
+                    egressToSet = new(),
+                    untaggedToUnset = new(),
+                    untaggedToSet = new();
+                if (!_commonData.NoPvid)
+                    pvidVariables.Add(new Variable(new ObjectIdentifier($"{OID_DOT1Q_PVID}.{port.Index + _commonData.PortIndexOffset}"), new Gauge32(vlan.ID)));
+                (int portByteIndex, int portBitIndex) = getByteBitIndex(port.Index + _commonData.PortIndexOffset);
+                await getVlansBitfieldsForPort(OID_DOT1Q_VLAN_STATIC_EGRESS_PORTS, vlan.ID, portByteIndex, portBitIndex, egressToSet, egressToUnset);
+                await getVlansBitfieldsForPort(OID_DOT1Q_VLAN_STATIC_UNTAGGED_PORTS, vlan.ID, portByteIndex, portBitIndex, untaggedToSet, untaggedToUnset);
+                await _variant.SetVariables(_snmpConnection, pvidVariables, egressToUnset, egressToSet, untaggedToUnset, untaggedToSet);
+                return true;
+            }
+
+            private async Task getVlansBitfieldsForPort(string tableObjectIdentifier, int targetVlanId, int portByteIndex, int portBitIndex, List<Variable> variablesToSet, List<Variable> variablesToUnset)
+            {
+                foreach (Variable oldRow in await _snmpConnection.WalkAsync(tableObjectIdentifier))
+                {
+                    SnmpVariableHelpers.IdParts idParts = oldRow.GetIdParts();
+                    if (idParts.RowId == 1)
+                        continue;
+                    bool valueToSet = idParts.RowId == targetVlanId;
+                    byte[] snmpDataBytes = (oldRow.Data as OctetString).GetRaw();
+                    snmpDataBytes.SetBit(portByteIndex, portBitIndex, valueToSet);
+                    Variable newRow = new(oldRow.Id, new OctetString(snmpDataBytes));
+                    (valueToSet ? variablesToSet : variablesToUnset).Add(newRow);
+                }
+            }
+
+            #region Variants
+            private static readonly IVariant[] VARIANTS_TO_REGISTER = new IVariant[]
+            {
+                new VariantCbs()
+            };
+
+            internal static readonly Dictionary<string, IVariant> VARIANTS = new();
+
+            static SetPortToVlanMethod()
+            {
+                foreach (IVariant variant in VARIANTS_TO_REGISTER)
+                    VARIANTS.Add(variant.Name, variant);
+            }
+
+            internal static IVariant DEFAULT_VARIANT = new VariantCbs();
+            #endregion
+
+        }
+    }
+}
---
+++ easyvlans-swopm-snmp-ciscovlanmemebership/Model/SwitchOperationMethods/SnmpMibCiscoVlanMemebership.cs
@@ -0,0 +1,81 @@
+﻿using BToolbox.XmlDeserializer.Attributes;
+using BToolbox.XmlDeserializer.Context;
+using System.Xml;
+
+namespace easyvlans.Model.SwitchOperationMethods
+{
+
+    internal sealed partial class SnmpMibCiscoVlanMemebership : ISnmpMib
+    {
+
+        public const string MIB_NAME = "ciscovlanmembership";
+
+        public class Deserializer : SnmpMibDeserializerBase
+        {
+
+            public override string ElementName => MIB_NAME;
+
+            protected override object createCommonData(XmlNode xmlNode, DeserializationContext context)
+            {
+                string setMembershipVariant = xmlNode.SelectSingleNode(DATA_TAG_SET_MEMBERSHIP_VARIANT)?.InnerText;
+                if (setMembershipVariant == null)
+                {
+                    context.Report(DeserializationReportSeverity.Info, xmlNode, "No variant defined for setting membership, using the default one.");
+                }
+                else
+                {
+                    if (!SetPortToVlanMethod.VARIANTS.TryGetValue(setMembershipVariant, out _))
+                    {
+                        context.Report(DeserializationReportSeverity.Warning, xmlNode, "Variant defined for setting membership not found, using the default one.");
+                        setMembershipVariant = null;
+                    }
+                }
+                return new CommonData()
+                {
+                    //NoPvid = (xmlNode.SelectNodes(DATA_TAG_NO_PVID).Count > 0),
+                    SetMembershipVariant = setMembershipVariant,
+                    PortIndexOffset = xmlNode.SelectSingleNode(DATA_TAG_PORT_INDEX_OFFSET)?.InnerAsInt(context).Min(0).Get().Value ?? 0
+                };
+            }
+
+            //public const string DATA_TAG_NO_PVID = "nopvid";
+            public const string DATA_TAG_PORT_INDEX_OFFSET = "port_index_offset";
+            public const string DATA_TAG_SET_MEMBERSHIP_VARIANT = "set_membership_variant";
+
+            protected override IReadVlanMembershipMethod createReadConfigMethod(ISnmpConnection snmpConnection, object commonData)
+                => new ReadVlanMembershipMethod(snmpConnection, commonData);
+
+            protected override ISetPortToVlanMethod createSetPortToVlanMethod(ISnmpConnection snmpConnection, object commonData)
+                => new SetPortToVlanMethod(snmpConnection, commonData);
+
+        }
+
+        internal class MethodBase : SnmpMethodBase
+        {
+
+            public override string MibName => MIB_NAME;
+
+            protected readonly CommonData _commonData;
+
+            public MethodBase(ISnmpConnection snmpConnection, object commonData)
+                : base(snmpConnection)
+                => _commonData = (CommonData)commonData;
+
+            protected static (int, int) getByteBitIndex(int portIndex) => ((portIndex - 1) / 8, 7 - (portIndex - 1) % 8);
+
+        }
+
+        internal class CommonData
+        {
+            //public bool NoPvid { get; init; }
+            public string SetMembershipVariant { get; init; }
+            public int PortIndexOffset { get; init; }
+        }
+
+        private const string OID_CISCOVLANMEMEBERSHIP_MEMBERSHIP_TABLE = "1.3.6.1.4.1.9.9.68.1.2.2";
+        private const string OID_CISCOVLANMEMEBERSHIP_TABLE_TYPE = $"{OID_CISCOVLANMEMEBERSHIP_MEMBERSHIP_TABLE}.1.1";
+        private const string OID_CISCOVLANMEMEBERSHIP_TABLE_VLAN = $"{OID_CISCOVLANMEMEBERSHIP_MEMBERSHIP_TABLE}.1.2";
+
+    }
+
+}
