3ce3358f2cf8eba59d73b3045bda737d7e885d2a
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2025-02-09 21:28:24 +0100

    Add RFC1213-MIB for reading switch uptime

---
+++ easyvlans-swopm-snmp-rfc1213/Model/SwitchOperationMethods/SnmpMibRfc1213.ReadSwitchUptimeMethod.cs
@@ -0,0 +1,25 @@
+﻿using Lextm.SharpSnmpLib;
+
+namespace easyvlans.Model.SwitchOperationMethods
+{
+    internal sealed partial class SnmpMibRfc1213
+    {
+        internal class ReadSwitchUptimeMethod : SnmpMethodBase, IReadSwitchUptimeMethod
+        {
+
+            public ReadSwitchUptimeMethod(ISnmpConnection snmpConnection) : base(snmpConnection) { }
+
+            public override string MibName => MIB_NAME;
+
+            public async Task DoAsync(Switch @switch)
+            {
+                IEnumerable<Variable> response = await _snmpConnection.GetAsync(new string[] { OID_SYS_UP_TIME });
+                Variable uptimeVariable = response.FirstOrDefault(v => v.Id.ToString() == OID_SYS_UP_TIME);
+                if (uptimeVariable == null)
+                    throw new Exception("'sysUpTime' object was not in the response.");
+                // update sysuptime of switch
+            }
+
+        }
+    }
+}
---
+++ easyvlans-swopm-snmp-rfc1213/Model/SwitchOperationMethods/SnmpMibRfc1213.cs
@@ -0,0 +1,23 @@
+﻿namespace easyvlans.Model.SwitchOperationMethods
+{
+
+    internal sealed partial class SnmpMibRfc1213 : ISnmpMib
+    {
+
+        public const string MIB_NAME = "rfc1213";
+
+        public class Deserializer : SnmpMibDeserializerBase
+        {
+
+            public override string ElementName => MIB_NAME;
+
+            protected override IReadSwitchUptimeMethod createReadSwitchUptimeMethod(ISnmpConnection snmpConnection, object commonData)
+                => new ReadSwitchUptimeMethod(snmpConnection);
+
+        }
+
+        private const string OID_SYS_UP_TIME = "1.3.6.1.2.1.1.3.0";
+
+    }
+
+}
